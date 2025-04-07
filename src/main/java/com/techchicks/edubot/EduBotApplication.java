6966ef4b8eea13a8a27fd7e3daaf5a0e1d3a98aa
migle.lukoseviciute <migle.lukoseviciute@visma.com>
2025-04-07 10:59:41 +0300

    1.4.3.1 connect database to the project

---
+++ src/main/java/com/techchicks/edubot/EduBotApplication.java
@@ -0,0 +1,13 @@
+package com.techchicks.edubot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techchicks/edubot/EduBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchicks.edubot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
