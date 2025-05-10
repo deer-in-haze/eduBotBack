548dc6a8471893fef2d41f378f007725d2fe8246
migle.lukoseviciute <migle.lukoseviciute@visma.com>
2025-05-10 22:28:07 +0300

    connect front

--- src/main/java/com/techchicks/edubot/config/WebMvcConfig.java
+++ src/main/java/com/techchicks/edubot/config/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package com.techchicks.edubot.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://edubot-front-8b601964ed4c.herokuapp.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowCredentials(true);
     }
